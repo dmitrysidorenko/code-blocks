f32976e7ba2f9647d866d787bf59e0be89d09e31
dmitrysidorenko <dmitriysidor3nk0@gmail.com>
2018-10-06 20:37:22 +0300

    star adding saved state

--- src/Console/dataCtx.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const ctx = React.createContext()
-const { Provider, Consumer } = ctx
-
-export { Provider, Consumer }
-
-export default ctx
--- src/index.js
+++ src/index.js
@@ -4,38 +4,66 @@ import Console from './Console'
 
 import './styles.css'
 
-function App() {
-  return (
-    <div className="App">
-      <button>+</button>
-      <div>
-        <Console initialValue="[1,2,3,4,5,6,7,8,9,10]">
-          {({ output }) => (
-            <Console
-              props={{ data: output }}
-              initialValue={`props.data
+const console1State = {
+  initialValue: '[1,2,3,4,5,6,7,8,9,10]',
+  child: {
+    initialValue: `props.data
+  .reduce((a, b) => a + b, 0)`,
+    child: {
+      initialValue: 'props.data / 5',
+      child: { initialValue: 'props.data / 3' }
+    }
+  }
+}
+const console2State = {
+  initialValue: 'props.data / 3',
+  props: { data: 24 }
+}
+
+class App extends React.Component {
+  state = {
+    blocks: [console1State, console2State]
+  }
+  addConsole = () => {}
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <Console
+            state={this.state.blocks[0]}
+            initialValue="[1,2,3,4,5,6,7,8,9,10]"
+          >
+            {({ output }) => (
+              <Console
+                props={{ data: output }}
+                initialValue={`props.data
   .reduce((a, b) => a + b, 0)`}
-            >
-              {({ output }) => (
-                <Console
-                  props={{ data: output }}
-                  initialValue="props.data + 9001"
-                >
-                  {({ output }) => (
-                    <Console
-                      props={{ data: output }}
-                      initialValue="props.data / 3"
-                    />
-                  )}
-                </Console>
-              )}
-            </Console>
-          )}
-        </Console>
-        <Console props={{ data: 1231212 }} initialValue="props.data / 3" />
+              >
+                {({ output }) => (
+                  <Console
+                    props={{ data: output }}
+                    initialValue="props.data + 9001"
+                  >
+                    {({ output }) => (
+                      <Console
+                        props={{ data: output }}
+                        initialValue="props.data / 3"
+                      />
+                    )}
+                  </Console>
+                )}
+              </Console>
+            )}
+          </Console>
+          <Console props={{ data: 1231212 }} initialValue="props.data / 3" />
+        </div>
+        <br />
+        <button onClick={this.addConsole} className="btn btn-plus">
+          + add block
+        </button>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 const rootElement = document.getElementById('root')
